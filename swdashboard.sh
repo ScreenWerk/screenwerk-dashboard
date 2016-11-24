705cb74644163f33e144681f4cd55165375a536c
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-11-24 20:33:00 +0200

    google timezone api

--- swdashboard.sh
+++ swdashboard.sh
@@ -31,6 +31,7 @@ docker run -d \
     --env="NEW_RELIC_LOG=stdout" \
     --env="NEW_RELIC_LOG_LEVEL=error" \
     --env="NEW_RELIC_NO_CONFIG_FILE=true" \
+    --env="GOOGLE_TIMEZONE_API_KEY=" \
     --env="NGINX_LOG=access.log" \
     --env="ENTU_KEY=" \
     --env="SENTRY_DSN=" \
